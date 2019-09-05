e5a50ea701af6c4a022113329f5be2a2062cfef7
Suzuki Mariko <>
2019-09-05 08:58:34 +0800

    エラー改正

--- Memoria/Controllers/DeletePhotosViewController.swift
+++ Memoria/Controllers/DeletePhotosViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
-import RealmSwift
 import Koloda
 
 class DeletePhotosViewController: UIViewController {
     
-    @IBOutlet weak var KokodaView: UIView!
+    @IBOutlet weak var KolodaView: UIView!
     
     let images = [
         "1",
@@ -24,7 +23,27 @@ class DeletePhotosViewController: UIViewController {
         "7",
         "8",
         "9",
-        "10"
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23",
+        "24",
+        "25",
+        "26",
+        "27",
+        "28",
+        "29",
+        "30"
     ]
     
     override func viewDidLoad() {
@@ -37,11 +56,35 @@ class DeletePhotosViewController: UIViewController {
     }
     
     @IBAction func didClickStay(_ sender: UIButton) {
+        KolodaView.
     }
     @IBAction func didClickDelete(_ sender: UIButton) {
     }
     @IBAction func didClickBack(_ sender: UIButton) {
+        KolodaView.revert
     }
     
 }
 
+extension ViewController: KolodaViewDelegate, KolodaViewDataSource {
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        //上でUIViewを返しているのに、下ではimageViewでエラーが発生しないのは、UIViewの中にimageViewが存在しているから
+        
+        // 画像を生成
+        let image = UIImage(named: images[index])
+        // ImageViewに生成した画像を設定
+        let imageView = UIImageView(image: image)
+        // ImageViewを返す
+        return imageView
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return images.count
+    }
+    
+    // この記述をすることで、上下左右どこにでもスワイプをすることを可能にすることができる！！！！！！！
+    // 逆にこれがないと不可能。
+    func koloda(_ koloda: KolodaView, allowedDirectionsForIndex index: Int) -> [SwipeResultDirection] {
+        return [.left, .right, .up, .down]
+    }
+}
